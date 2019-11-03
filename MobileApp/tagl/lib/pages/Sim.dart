e6d133db271e9d2469c79c78e446b08028c17e80
Andres Gutierrez <andygtz675@gmail.com>
2019-11-03 12:37:01 -0600

    wip sent wav

--- MobileApp/tagl/lib/pages/Sim.dart
+++ MobileApp/tagl/lib/pages/Sim.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:audio_recorder/audio_recorder.dart';
 import 'package:file/local.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:tagl/pages/Evaluation.dart';
 import 'package:tagl/utilities/AppColors.dart';
 import 'package:tagl/utilities/FontStyles.dart';
@@ -11,6 +12,7 @@ import 'package:http_parser/http_parser.dart';
 import 'package:path/path.dart';
 import 'dart:convert';
 import 'package:async/async.dart';
+import 'package:flutter_uploader/flutter_uploader.dart';
 
 class Sim extends StatefulWidget {
   @override
@@ -21,6 +23,8 @@ class _SimState extends State<Sim> {
   bool isRecording = false;
   LocalFileSystem localFileSystem;
   Recording recording = Recording();
+  File defaultAudioFile;
+  String tempFilename = "audioFile.wav";
 
   startRecording() async {
     await AudioRecorder.start(
@@ -31,42 +35,78 @@ class _SimState extends State<Sim> {
 
   stopRecording() async {
     recording = await AudioRecorder.stop();
+    Directory directory = await getApplicationDocumentsDirectory();
+
     isRecording = await AudioRecorder.isRecording;
     print(
         "Path : ${recording.path},  Format : ${recording.audioOutputFormat},  Duration : ${recording.duration},  Extension : ${recording.extension},");
+
     setState(() {});
   }
 
+  // submitRecording() async {
+  //   if (recording == null) {
+  //     return;
+  //   } else if (isRecording) {
+  //     stopRecording();
+  //   }
+
+  //   // print("aklsjdfhkasjfhkl");
+
+  //   // final directory = await getApplicationDocumentsDirectory();
+
+  //   // String recordingPath = recording.path.split("/")[1];
+
+  //   // // print(directory.path);
+
+  //   // File localFile = File('${directory.path}/$recordingPath');
+
+  //   // print(localFile);
+
+  //   // Read the file.
+
+  //   File rf = localFile;
+
+  //   var url = "http://tomas3torres.com:5000/wav-to-flac";
+
+  //   http.MultipartFile multipartFile = http.MockMultipartFile("test.xlsx", new FileInputStream(new File("/home/admin/test.xlsx")));
+
+  //   var stream = new http.ByteStream(DelegatingStream.typed(rf.openRead()));
+  //   var length = await rf.length();
+  //   var request = new http.MultipartRequest("POST", Uri.parse(url));
+  //   var multipartFile = new http.MultipartFile(
+
+  //   );
+  //   request.files.add(multipartFile);
+  //   request.send().then((response) async {
+  //     String responseString = await response.stream.bytesToString();
+  //     print(responseString);
+
+  //     // Map<String, dynamic> responseJSON = jsonDecode(responseString);
+  //     // String submission = responseJSON['submission'];
+
+  //     print("THINGYYY");
+
+  //     print(rf.path);
+  //   });
+  // }
+
   submitRecording() async {
-    if (recording == null) {
-      return;
-    } else if (isRecording) {
-      stopRecording();
-    }
+    String url = "http://tomas3torres.com:5000/wav-to-flac";
 
-    print("aklsjdfhkasjfhkl");
+    final uploader = FlutterUploader();
 
-    File rf = File('recording.wav');
-
-    var url = "http://tomas3torres.com:5000/wav-to-flac";
-
-    var stream = new http.ByteStream(DelegatingStream.typed(rf.openRead()));
-    var length = await rf.length();
-    var request = new http.MultipartRequest("POST", Uri.parse(url));
-    var multipartFile = new http.MultipartFile('wavFile', stream, length,
-        filename: basename(rf.path));
-    request.files.add(multipartFile);
-    request.send().then((response) async {
-      String responseString = await response.stream.bytesToString();
-      print(responseString);
-
-      // Map<String, dynamic> responseJSON = jsonDecode(responseString);
-      // String submission = responseJSON['submission'];
-
-      print("THINGYYY");
-
-      print(rf.path);
-    });
+    final taskId = await uploader.enqueue(
+        url: "your upload link", //required: url to upload to
+        files: [
+          FileItem(filename: "wavFile", savedDir: "savedDir", fieldname: "file")
+        ], // required: list of files that you want to upload
+        method: UploadMethod.POST, // HTTP method  (POST or PUT or PATCH)
+        headers: {"apikey": "api_123456", "userkey": "userkey_123456"},
+        data: {"name": "john"}, // any data you want to send in upload request
+        showNotification:
+            false, // send local notification (android only) for upload status
+        tag: "upload 1"); // unique tag for upload task
   }
 
   @override
